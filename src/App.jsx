455a3d952fa4bd5d7bc7817f1857e200f0241924
GopiThangarajTR <gopi.thangaraj@thomsonreuters.com>
2024-10-14 16:33:37 +0530

    Updated header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PullRequestDate from './components/PullRequestDate'
 function App() {
   return (
     <>
-     <h1 className="text-3xl mb-8 text-center">Release Version</h1>
+     <h1 className="text-3xl mb-8 text-center">Release Versions</h1>
      <PullRequestDate />
     </>
   )
